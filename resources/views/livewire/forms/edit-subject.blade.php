b3949865d2415076ce9d58afa7cb5f7dfc8b35ac
Dusty Noble <dnoble82581@gmail.com>
2024-12-26 07:27:32 -0600

    Added Subject Edit Form

--- resources/views/livewire/forms/edit-subject.blade.php
+++ resources/views/livewire/forms/edit-subject.blade.php
@@ -10,91 +10,91 @@ new class extends Component {
 }
 
 ?>
+<div>
+	<x-form-layouts.form-layout>
+		<x-slot:header>
+			Edit Subject
+		</x-slot:header>
+		<x-slot:description>
+			Non sint laboriosam ad perferendis soluta est quae consequatur et quae soluta qui exercitationem earum qui
+			officiis consequatur sit dicta laboriosam.
+		</x-slot:description>
+		<x-slot:actions>
+			<x-buttons.primary-button>
+				Save
+			</x-buttons.primary-button>
+			<x-buttons.secondary-button>
+				Cancel
+			</x-buttons.secondary-button>
+		</x-slot:actions>
+		<div class="flex flex-col sm:flex-row items-center gap-4">
+			<x-input
+					wiremodel="form.name"
+					label=" Subject Name"
+					placeholder="Subject name" />
+			<x-input
+					wire:model="form.email"
+					label="Email Address"
+					placeholder="Subject Email" />
+			<x-phone
+					wire:model="form.phone"
+					label="Phone Number"
+					:mask="['(###) ###-####', '+# ### ###-####', '+## ## ####-####']" />
 
-<x-form-layouts.form-layout>
-	<x-slot:header>
-		Edit Subject
-	</x-slot:header>
-	<x-slot:description>
-		Non sint laboriosam ad perferendis soluta est quae consequatur et quae soluta qui exercitationem earum qui
-		officiis consequatur sit dicta laboriosam.
-	</x-slot:description>
-	<x-slot:actions>
-		<x-buttons.primary-button>
-			Save
-		</x-buttons.primary-button>
-		<x-buttons.secondary-button>
-			Cancel
-		</x-buttons.secondary-button>
-	</x-slot:actions>
-	<div class="flex flex-col sm:flex-row items-center gap-4">
-		<x-input
-				wiremodel="form.name"
-				label=" Subject Name"
-				placeholder="Subject name" />
-		<x-input
-				wire:model="form.email"
-				label="Email Address"
-				placeholder="Subject Email" />
-		<x-phone
-				wire:model="form.phone"
-				label="Phone Number"
-				:mask="['(###) ###-####', '+# ### ###-####', '+## ## ####-####']" />
+		</div>
 
-	</div>
-
-	<div class="flex flex-col sm:flex-row items-center gap-4">
-		<x-input
-				wire:model="form.address"
-				label="Address"
-				placeholder="Subject Address" />
-	</div>
+		<div class="flex flex-col sm:flex-row items-center gap-4">
+			<x-input
+					wire:model="form.address"
+					label="Address"
+					placeholder="Subject Address" />
+		</div>
 
 
-	<div class="flex flex-col sm:flex-row items-center gap-4">
-		<x-input
-				wire:model="form.city"
-				label="City"
-				placeholder="Subject City" />
-		<x-input
-				wire:model="form.state"
-				label="State"
-				placeholder="Subject State" />
-		<x-input
-				wire:model="form.zip"
-				label="Zip"
-				placeholder="Subject Zip" />
-	</div>
+		<div class="flex flex-col sm:flex-row items-center gap-4">
+			<x-input
+					wire:model="form.city"
+					label="City"
+					placeholder="Subject City" />
+			<x-input
+					wire:model="form.state"
+					label="State"
+					placeholder="Subject State" />
+			<x-input
+					wire:model="form.zip"
+					label="Zip"
+					placeholder="Subject Zip" />
+		</div>
 
-	<div class="flex flex-col sm:flex-row items-center gap-4">
-		<x-datetime-picker
-				label="Date of Birth"
-				wire:model="form.date_of_birth"
-				:max="now()"
-				without-time="true" />
-		<x-input
-				wire:model="form.age"
-				label="Age"
-				placeholder="Subject Age" />
+		<div class="flex flex-col sm:flex-row items-center gap-4">
+			<x-datetime-picker
+					label="Date of Birth"
+					wire:model="form.date_of_birth"
+					:max="now()"
+					without-time="true" />
+			<x-input
+					wire:model="form.age"
+					label="Age"
+					placeholder="Subject Age" />
 
-		<x-select
-				wire:model="form.substance_abuse"
-				label="Substance Abuse History"
-				placeholder="Substance Abuse"
-				:options="['Yes', 'No', 'Unknown']" />
+			<x-select
+					wire:model="form.substance_abuse"
+					label="Substance Abuse History"
+					placeholder="Substance Abuse"
+					:options="['Yes', 'No', 'Unknown']" />
 
-	</div>
-	<div class="flex flex-col sm:flex-row items-center gap-4">
-		<x-select
-				wire:model="form.mental_health_history"
-				label="Mental Health History"
-				placeholder="Mental Health History"
-				:options="['Yes', 'No', 'Unknown']" />
-		<x-select
-				wire:model="form.race"
-				label="Subject Race"
-				placeholder="Subject Race"
-				:options="[
+		</div>
+		<div class="flex flex-col sm:flex-row items-center gap-4">
+			<x-select
+					wire:model="form.mental_health_history"
+					label="Mental Health History"
+					placeholder="Mental Health History"
+					:options="['Yes', 'No', 'Unknown']" />
+			<x-select
+					wire:model="form.race"
+					label="Subject Race"
+					placeholder="Subject Race"
+					:options="[
 					['name' => 'White', 'id' => 'White', 'description' => '(Europe, Middle East, North Africa)'],
 					['name' => 'Mongoloid', 'id' => 'Mongoloid', 'description' => '(East Asia, Central Asia, Indigenous peoples of the Americas)'],
 					['name' => 'Negroid', 'id' => 'Negroid', 'description' => '(Sub-Saharan Africa)'],
@@ -102,59 +102,58 @@ new class extends Component {
 					['name' => 'Mixed-race', 'id' => 'Mixed-race', 'description' => '(People with heritage from multiple groups)'],
 					['name' => 'Other Ethnic Groupings', 'id' => 'Other Ethnic Groupings', 'description' => '(South Asian, Middle Eastern, etc.)'],
 				]"
-				option-label="name"
-				option-value="id" />
-		<x-select
-				wire:model="form.gender"
-				label="Subject Gender"
-				:options="['Male', 'Female', 'Transgender', 'Unknown']" />
-	</div>
-	<div class="flex flex-col sm:flex-row items-center gap-4">
-
-		<x-select
-				wire:model="form.children"
-				label="Children"
-				:options="[1,2,3,4,5,6,7,8,9,10]" />
-		<x-select
-				wire:model="form.veteran"
-				label="Veteran"
-				:options="['Yes', 'No', 'Unknown']" />
-		<x-select
-				wire:model="form.highest_education"
-				label="Highest Education"
-				placeholder="Highest Education"
-				:options="['Grade School', 'High School', 'College', 'Graduate', 'Unknown']" />
-	</div>
-	<div class="flex flex-col sm:flex-row items-center gap-4">
-		<x-input
-				label="Facebook Url"
-				placeholder="Facebook Url"
-				wire:model="form.facebook_url" />
-		<x-input
-				label="Instagram Url"
-				placeholder="Instagram Url"
-				wire:model="form.instagram_url" />
-		<x-input
-				label="X Url"
-				placeholder="X Url"
-				wire:model="form.x_url" />
-		<x-input
-				label="Snapchat Url"
-				placeholder="Snapchat Url"
-				wire:model="form.snapchat_url" />
-	</div>
-	<div>
-		<div class="h-28 grid grid-cols-12 gap-2">
-			@if ($this->form->images)
-				@foreach ($this->form->images as $image)
-					<img
-							alt="Image"
-							src="{{ $image->temporaryUrl() }}"
-							class="h-28 w-28 col-span-1 object-cover">
-				@endforeach
-			@endif
+					option-label="name"
+					option-value="id" />
+			<x-select
+					wire:model="form.gender"
+					label="Subject Gender"
+					:options="['Male', 'Female', 'Transgender', 'Unknown']" />
 		</div>
-		<div class="">
+		<div class="flex flex-col sm:flex-row items-center gap-4">
+
+			<x-select
+					wire:model="form.children"
+					label="Children"
+					:options="[1,2,3,4,5,6,7,8,9,10]" />
+			<x-select
+					wire:model="form.veteran"
+					label="Veteran"
+					:options="['Yes', 'No', 'Unknown']" />
+			<x-select
+					wire:model="form.highest_education"
+					label="Highest Education"
+					placeholder="Highest Education"
+					:options="['Grade School', 'High School', 'College', 'Graduate', 'Unknown']" />
+		</div>
+		<div class="flex flex-col sm:flex-row items-center gap-4">
+			<x-input
+					label="Facebook Url"
+					placeholder="Facebook Url"
+					wire:model="form.facebook_url" />
+			<x-input
+					label="Instagram Url"
+					placeholder="Instagram Url"
+					wire:model="form.instagram_url" />
+			<x-input
+					label="X Url"
+					placeholder="X Url"
+					wire:model="form.x_url" />
+			<x-input
+					label="Snapchat Url"
+					placeholder="Snapchat Url"
+					wire:model="form.snapchat_url" />
+		</div>
+		<div>
+			<div class="h-28 flex gap-2">
+				@if ($this->form->images)
+					@foreach ($this->form->images as $image)
+						<img
+								alt="Image"
+								src="{{ $image->temporaryUrl() }}"
+								class="h-28 w-28 col-span-1 object-cover">
+					@endforeach
+				@endif
+			</div>
 
 			<div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
 				<div class="text-center">
@@ -188,8 +187,9 @@ new class extends Component {
 				</div>
 			</div>
 		</div>
-	</div>
-</x-form-layouts.form-layout>
+	</x-form-layouts.form-layout>
+</div>
+
 
 {{--
 name *
