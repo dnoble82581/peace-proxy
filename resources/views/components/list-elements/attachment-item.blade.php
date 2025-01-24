37b056daba79188cb8c52855edd227a2a5b24b8d
Dusty Noble <dnoble82581@gmail.com>
2025-01-24 13:47:11 -0600

    Got documents working for both user and subject

--- resources/views/components/list-elements/attachment-item.blade.php
+++ resources/views/components/list-elements/attachment-item.blade.php
@@ -1,7 +1,7 @@
 @props(['document' => null])
 <li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm/6">
 	<div class="flex w-0 flex-1 items-center">
-		<x-svg-images.paper-clip />
+		<x-heroicons::outline.paper-clip class="shrink-0 h-5 w-5 text-gray-400" />
 		<div class="ml-4 flex min-w-0 flex-1 gap-2">
 			<span class="truncate font-medium capitalize">{{ $document->offense ?? $document->filename}}</span>
 			<span class="shrink-0 text-gray-400">2.4mb</span>
