be1a7cbfbf23fb121e50627acb970ae9635e19ff
Dusty Noble <dnoble82581@gmail.com>
2025-03-08 13:24:58 -0600

    testing

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -7,5 +7,5 @@ Route::view('dashboard/team', 'pages.team')->name('team');
 Route::view('create-user', 'pages.create-user')->name('create.user');
 Route::get('dashboard/team/edit-user/{id}', [UserController::class, 'update'])->name('edit.user');
 Route::get('dashboard', [PagesController::class, 'dashboard'])
-    ->middleware(['verified', 'subscribed'])
+    ->middleware(['verified'])
     ->name('dashboard');
